7c1ac25bd88bbc4d22b0219261b99c7ec6e34e1e
CharlieWhiting1 <P2712463@my365.dmu.ac.uk>
2024-05-02 09:39:35 +0100

    Added testing for User class

---
+++ Testing2/tstUser.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Create an instance of the class we want to create
+            clsUser AnAddress = new clsUser();
+            // test to see that it exists
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
