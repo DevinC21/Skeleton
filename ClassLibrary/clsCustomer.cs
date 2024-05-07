32bfae9e1f609e56d62077bec360dc16835439e2
CharlieWhiting1 <P2712463@my365.dmu.ac.uk>
2024-05-07 13:59:06 +0100

    Added testing for Customer class

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCustomer
+    {
+        public bool CustomerConfirmed { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CustomerId { get; set; }
+        public int BankDetails { get; set; }
+        public string CustomerUsername { get; set; }
+        public string CustomerPass { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,102 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Assert.IsNotNull(AnCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerConfirmedPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Boolean TestData = true;
+
+            AnCustomer.CustomerConfirmed = TestData;
+
+            Assert.AreEqual(AnCustomer.CustomerConfirmed, TestData);
+        }
+
+        [TestMethod]
+        public void DateOfBirthPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            AnCustomer.DateOfBirth = TestData;
+
+            Assert.AreEqual(AnCustomer.DateOfBirth, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Int32 TestData = 1;
+
+            AnCustomer.CustomerId = TestData;
+
+            Assert.AreEqual(AnCustomer.CustomerId, TestData);
+        }
+
+        [TestMethod]
+        public void BankDetailsPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Int32 TestData = 1;
+
+            AnCustomer.BankDetails = TestData;
+
+            Assert.AreEqual(AnCustomer.BankDetails, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerUsernamePropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            string TestData = "Charlie";
+
+            AnCustomer.CustomerUsername = TestData;
+
+            Assert.AreEqual(AnCustomer.CustomerUsername, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerPassPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            string TestData = "Password";
+
+            AnCustomer.CustomerPass = TestData;
+
+            Assert.AreEqual(AnCustomer.CustomerPass, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerEmailPropertyOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            string TestData = "Charlie";
+
+            AnCustomer.CustomerEmail = TestData;
+
+            Assert.AreEqual(AnCustomer.CustomerEmail, TestData);
+        }
+    }
+}
