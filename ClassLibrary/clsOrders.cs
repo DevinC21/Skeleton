be94645836f5bc355e974e60b7838d9acbb07c52
Devin Chohan <P2716238@my365.dmu.ac.uk>
2024-05-08 10:54:48 +0100

    i have made changes

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrders
     {
+        public bool Payment { get; set; }
+        public int OrderID { get; set; }
+        public string OrderFullName { get; set; }
+        public string OrderDescription { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderReturn { get; set; }
+        public string OrderStatus { get; set; }
     }
 }
