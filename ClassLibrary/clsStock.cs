0dc9e6b5ed6b27636df65e9285330d1bc8bba15a
p2543336 <p2543336@my365dmu.ac.uk>
2024-05-15 14:17:34 +0100

    practical 8

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,9 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
-        public clsStock()
-        {
-        }
+        public int StockID { get; set; }
+        public string Brand { get; set; }
+        public string Colour { get; set; }
+        public string TypeOfCar { get; set; }
+        public bool Gearbox { get; set; }
+        public DateTime YearOfCar { get; set; }
+        public int Prices { get; set; }
     }
 }
