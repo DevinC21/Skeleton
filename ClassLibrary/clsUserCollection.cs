b2d720fe3d1bda0914c4d23c19512fa20e0175d5
MazdaZohaib <p2712845@my365.dmu.ac.uk>
2024-05-18 17:13:21 +0100

    practical 12

---
+++ ClassLibrary/clsUserCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsUserCollection
+    {
+        //private data member for the list
+        List<clsUser> mUserList = new List<clsUser>();
+
+        //public property for the user list
+        public List<clsUser> UserList
+        {
+            get
+            {
+                //return the private data
+                return mUserList;
+            }
+            set
+            {
+                //set the private data
+                mUserList = value;
+            }
+        }
+
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mUserList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsUser ThisUser { get; set; }
+
+        //constructor for the class
+        public clsUserCollection()
+        {
+            //variable for the index
+            Int32 Index = 0;
+
+            //variable to store the record count
+            Int32 RecordCount = 0;
+
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+
+            //execute the stored procedure
+            DB.Execute("sproc_tblUser_SelectAll");
+
+            //get the count of records
+            RecordCount = DB.Count;
+
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank user
+                clsUser AnUser = new clsUser();
+
+                //read in the fields for the current record
+                AnUser.LoggedIn = Convert.ToBoolean(DB.DataTable.Rows[Index]["LoggedIn"]);
+                AnUser.UserID = Convert.ToInt32(DB.DataTable.Rows[Index]["UserID"]);
+                AnUser.UserContactNumber = Convert.ToInt32(DB.DataTable.Rows[Index]["UserContactNumber"]);
+                AnUser.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                AnUser.UserName = Convert.ToString(DB.DataTable.Rows[Index]["UserName"]);
+                AnUser.UserPrivileges = Convert.ToString(DB.DataTable.Rows[Index]["UserPrivileges"]);
+                AnUser.UserDob = Convert.ToDateTime(DB.DataTable.Rows[Index]["UserDob"]);
+
+                //add the record to the private data member
+                mUserList.Add(AnUser);
+
+                //point at the next record
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing2/tstUserCollection.cs
@@ -0,0 +1,112 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstUserCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //test to see that it exists
+            Assert.IsNotNull(AllUsers);
+        }
+
+        [TestMethod]
+        public void UserListOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsUser> TestList = new List<clsUser>();
+
+            //add an item to the list
+            //create the item of test data
+            clsUser TestItem = new clsUser();
+
+            //set its properties
+            TestItem.LoggedIn = true;
+            TestItem.UserID = 1;
+            TestItem.UserContactNumber = 777777777;
+            TestItem.CustomerID = 1;
+            TestItem.UserName = "MazdaZohaib";
+            TestItem.UserPrivileges = "Admin";
+            TestItem.UserDob = DateTime.Now;
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            AllUsers.UserList = TestList;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.UserList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisUserPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //create some test data to assign to the property
+            clsUser TestUser = new clsUser();
+
+            //set the properties of the test object
+            TestUser.LoggedIn = true;
+            TestUser.UserID = 1;
+            TestUser.UserContactNumber = 777777777;
+            TestUser.CustomerID = 1;
+            TestUser.UserName = "MazdaZohaib";
+            TestUser.UserPrivileges = "Admin";
+            TestUser.UserDob = DateTime.Now;
+
+            //assign the data to the property
+            AllUsers.ThisUser = TestUser;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.ThisUser, TestUser);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsUser> TestList = new List<clsUser>();
+
+            //add an item to the list
+            //create the item of test data
+            clsUser TestItem = new clsUser();
+
+            //set its properties
+            TestItem.LoggedIn = true;
+            TestItem.UserID = 1;
+            TestItem.UserContactNumber = 777777777;
+            TestItem.CustomerID = 1;
+            TestItem.UserName = "MazdaZohaib";
+            TestItem.UserPrivileges = "Admin";
+            TestItem.UserDob = DateTime.Now;
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            AllUsers.UserList = TestList;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.Count, TestList.Count);
+        }
+    }
+}
