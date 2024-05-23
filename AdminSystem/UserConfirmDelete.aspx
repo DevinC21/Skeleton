982d136b83683be57cc59634600bbef4dc73a6bd
MazdaZohaib <p2712845@my365.dmu.ac.uk>
2024-05-23 17:27:38 +0100

    Practical 14a and 14b

--- AdminSystem/UserConfirmDelete.aspx
+++ AdminSystem/UserConfirmDelete.aspx
@@ -7,5 +7,10 @@
     <title></title>
 </head>
 <body>
+    <form id="form1" runat="server">
+        <asp:Label ID="lblDelete" runat="server" style="z-index: 1; left: 10px; top: 15px; position: absolute" Text="Are you sure you want to delete this record?"></asp:Label>
+        <asp:Button ID="btnYes" runat="server" style="z-index: 1; left: 55px; top: 60px; position: absolute; width: 45px; right: 504px;" Text="Yes" OnClick="btnYes_Click" />
+        <asp:Button ID="btnNo" runat="server" style="z-index: 1; left: 135px; top: 60px; position: absolute; width: 40px;" Text="No" OnClick="btnNo_Click" />
+    </form>
     </body>
 </html>
--- Testing2/tstUserCollection.cs
+++ Testing2/tstUserCollection.cs
@@ -127,7 +127,7 @@ namespace Testing2
             TestItem.UserContactNumber = 777777777;
             TestItem.CustomerID = 1;
             TestItem.UserName = "Suiiiiiii";
-            TestItem.UserPrivileges = "Admin";
+            TestItem.UserPrivileges = " Adds";
             TestItem.UserDob = DateTime.Now;
 
             //set ThisUser to the test data
@@ -160,11 +160,10 @@ namespace Testing2
 
             //set its properties
             TestItem.LoggedIn = true;
-            TestItem.UserID = 1;
             TestItem.UserContactNumber = 777777777;
             TestItem.CustomerID = 1;
             TestItem.UserName = "Suiiiiiii";
-            TestItem.UserPrivileges = "User";
+            TestItem.UserPrivileges = "Edit";
             TestItem.UserDob = DateTime.Now.AddYears(-12);
 
             //set ThisUser to the test data
@@ -178,11 +177,10 @@ namespace Testing2
 
             //modify the test record
             TestItem.LoggedIn = false;
-            TestItem.UserID = 12;
             TestItem.UserContactNumber = 75726725;
             TestItem.CustomerID = 12;
             TestItem.UserName = "Unsuiiiiiii";
-            TestItem.UserPrivileges = "User";
+            TestItem.UserPrivileges = "Edits";
             TestItem.UserDob = DateTime.Now.AddYears(-20);
 
             //set the record based on the new test data
@@ -196,6 +194,112 @@ namespace Testing2
 
             //test to see that the two values are the same
             Assert.AreEqual(AllUsers.ThisUser, TestItem);
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //create the item of test data
+            clsUser TestItem = new clsUser();
+
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+
+            //set its properties
+            TestItem.LoggedIn = true;
+            TestItem.UserID = 1;
+            TestItem.UserContactNumber = 777777777;
+            TestItem.CustomerID = 1;
+            TestItem.UserName = "Suiiiiiii";
+            TestItem.UserPrivileges = "Admin";
+            TestItem.UserDob = DateTime.Now;
+
+            //set ThisUser to the test data
+            AllUsers.ThisUser = TestItem;
+
+            //add the record
+            PrimaryKey = AllUsers.Add();
+
+            //set the primary key of the test data
+            TestItem.UserID = PrimaryKey;
+
+            //find the record
+            AllUsers.ThisUser.Find(PrimaryKey);
+
+            //deletd the record
+            AllUsers.Delete();
+
+            //now find the record
+            Boolean Found = AllUsers.ThisUser.Find(PrimaryKey);
+
+            //test to see that the two values are the same
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void ReportByUserNameMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsUserCollection AllUsers = new clsUserCollection();
+
+            //create an instance of the filtered data
+            clsUserCollection FilteredUsers = new clsUserCollection();
+
+            //apply a blank string (should return all records)
+            FilteredUsers.ReportByUserName("");
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.Count, FilteredUsers.Count);
+        }
+
+        [TestMethod]
+        public void ReportByUserNameNoneFound()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection FilteredUsers = new clsUserCollection();
+
+            //apply a username that doesn't exist
+            FilteredUsers.ReportByUserName("xxxxx");
+
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredUsers.Count);
+        }
+
+        [TestMethod]
+        public void ReportByUserNameTestDataFound()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection FilteredUsers = new clsUserCollection();
+
+            //variable to store the outcome
+            Boolean OK = true;
+
+            //apply a username that doesn't exist
+            FilteredUsers.ReportByUserName("Hello");
+
+            //check that the correct number of records are found
+            if (FilteredUsers.Count == 2)
+            {
+                //check to see that the first record is 25
+                if (FilteredUsers.UserList[0].UserID != 25)
+                {
+                    OK = false;
+                }
+
+                //check to see that the first record is 26
+                if (FilteredUsers.UserList[0].UserID != 26)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                //test to see that there are no records
+                Assert.IsTrue(OK);
+            }
         }
     }
 }
