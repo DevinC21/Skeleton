96a290a3ebbad8768f46cca890952d65972d2d83
MazdaZohaib <p2712845@my365.dmu.ac.uk>
2024-05-28 15:00:32 +0100

    practical 15

---
+++ ClassLibrary/clsUserLogin.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsUserLogin
+    {
+        //private data member for the login id property
+        private Int32 mLoginID;
+
+        //private data member for the login name property
+        private string mLoginName;
+
+        //private data member for the login password property
+        private string mLoginPassword;
+
+        //private data member for the login department property
+        private string mLoginDepartment;
+
+        public int LoginID
+        {
+            get
+            {
+                //return the private data
+                return mLoginID;
+            }
+            set
+            {
+                //set the private data
+                mLoginID = value;
+            }
+        }
+
+        public string LoginName
+        {
+            get
+            {
+                //return the private data
+                return mLoginName;
+            }
+            set
+            {
+                //set the private data
+                mLoginName = value;
+            }
+        }
+
+        public string LoginPassword
+        {
+            get
+            {
+                //return the private data
+                return mLoginPassword;
+            }
+            set
+            {
+                //set the private data
+                mLoginPassword = value;
+            }
+        }
+
+        public string LoginDepartment
+        {
+            get
+            {
+                //return the private data
+                return mLoginDepartment;
+            }
+            set
+            {
+                //set the private data
+                mLoginDepartment = value;
+            }
+        }
+
+        public bool FindUser(string LoginName, string LoginPassword)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameters for the user login name and login password to search for
+            DB.AddParameter("@LoginName", LoginName);
+            DB.AddParameter("@LoginPassword", LoginPassword);
+            //execute the stored procedure
+            DB.Execute("sproc_tblLogin_FindLoginNamePW");
+            //if one record is found (there should be either one or none)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mLoginID = Convert.ToInt32(DB.DataTable.Rows[0]["LoginID"]);
+                mLoginName = Convert.ToString(DB.DataTable.Rows[0]["LoginName"]);
+                mLoginPassword = Convert.ToString(DB.DataTable.Rows[0]["LoginPassword"]);
+                mLoginDepartment = Convert.ToString(DB.DataTable.Rows[0]["LoginDepartment"]);
+                //return true to confirm everything worked ok
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Testing2/tstUserLogin.cs
@@ -0,0 +1,132 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstUserLogin
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the new class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //test to see that it exists
+            Assert.IsNotNull(AnLogin);
+        }
+
+        [TestMethod]
+        public void LoginIDPropertyOK()
+        {
+            //create an instance of the new class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+
+            //assign the data to the property
+            AnLogin.LoginID = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnLogin.LoginID, TestData);
+        }
+
+        [TestMethod]
+        public void LoginNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create some test data to assign to the property
+            string TestData = "MazdaZohaib";
+
+            //assign the data to the property
+            AnLogin.LoginName = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnLogin.LoginName, TestData);
+        }
+
+        [TestMethod]
+        public void LoginPasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create some test data to assign to the property
+            string TestData = "password123";
+
+            //assign the data to the property
+            AnLogin.LoginPassword = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnLogin.LoginPassword, TestData);
+        }
+
+        [TestMethod]
+        public void LoginDepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create some test data to assign to the property
+            string TestData = "User";
+
+            //assign the data to the property
+            AnLogin.LoginDepartment = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnLogin.LoginDepartment, TestData);
+        }
+
+        [TestMethod]
+        public void FindLoginMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+
+            //create some test data to use with the method
+            string LoginName = "MazdaZohaib";
+            string LoginPassword = "password123";
+
+            //invoke the method
+            Found = AnLogin.FindUser(LoginName, LoginPassword);
+
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestLoginNamePWFound()
+        {
+            //create an instance of the class we want to create
+            clsUserLogin AnLogin = new clsUserLogin();
+
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+
+            //create a Boolean variable to record if the data is OK (assume it is)  
+            Boolean OK = true;
+
+            //create some test data to use with the method
+            string LoginName = "MazdaZohaib";
+            string LoginPassword = "password123";
+
+            //invoke the method
+            Found = AnLogin.FindUser(LoginName, LoginPassword);
+
+            //check the Login ID property
+            if (AnLogin.LoginName != LoginName && AnLogin.LoginPassword != LoginPassword)
+            {
+                OK = false;
+            }
+            //test to see if the result is true
+            Assert.IsTrue(OK);
+        }
+    }
+}
